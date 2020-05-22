04bdff8fb38ae2a461d0aea6be50813bb69ea76b
tiaraintana <tiaraintana@gmail.com>
2020-05-22 21:11:23 +0700

    fitur log-out

--- app/src/main/java/com/example/system_login/MenuAfterLogin.java
+++ app/src/main/java/com/example/system_login/MenuAfterLogin.java
@@ -2,13 +2,31 @@ package com.example.system_login;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.bumptech.glide.Glide;
+import com.example.system_login.model.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MenuAfterLogin extends AppCompatActivity {
     ImageView Daftar, TtgProgram, HalamanBerita, scanner, RekapData;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,7 +78,22 @@ public class MenuAfterLogin extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
 
-
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.logout:
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(MenuAfterLogin.this, MainActivity.class));
+                finish();
+                return true;
+        }
+        return false;
     }
 }
